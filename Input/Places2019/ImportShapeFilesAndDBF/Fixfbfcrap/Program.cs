816021ac548e33d74caa19fff36683f1760c482e
Magical Marvelous MADMADMAD Mister Mim ! <john.r.sohn@gmail.com>
2020-09-15 07:03:04 -0600

    Pre backup commit

--- Input/Places2019/ImportShapeFilesAndDBF/Fixfbfcrap/Program.cs
+++ Input/Places2019/ImportShapeFilesAndDBF/Fixfbfcrap/Program.cs
@@ -15,33 +15,9 @@ namespace Fixfbfcrap
         {
             string sampleshpfile = @"C:\Users\John\Documents\QrCode\Input\Places2019\Places\tl_2019_01_place.shp";
 
-            FileStream fs = new FileStream(sampleshpfile, FileMode.Open);
-            BinaryReader br = new BinaryReader(fs);
+            ShapeFile s = new ShapeFile(sampleshpfile);
 
-            // they're anticipating me reading in a certain number of bytes.
-
-            SHPHeader sh = new SHPHeader(br);
-
-            while (br.BaseStream.Position < br.BaseStream.Length)
-            {
-                RecordHeader rh = new RecordHeader(br);
-            }
-
-            br.Close();
-
-
-           // previou stub generation code.
-           //var names =  Enum.GetNames(typeof (SHPHeader.eShapeType));
-
-           // foreach (string s in names)
-           // {
-           //     if (s!="NullShape")
-           //     {
-           //        string content =  File.ReadAllText(@"..\..\NullShape.cs").Replace("Null", s);
-           //        File.WriteAllText(s + "Shape.cs",content);
-           //     }
-           // }
-                
+            s.Load();
            
         }
     }
