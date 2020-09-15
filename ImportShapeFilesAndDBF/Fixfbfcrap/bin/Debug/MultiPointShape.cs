92c1cd60f6f855bedf7192ab5a0a0dc7e3677784
Magical Marvelous MADMADMAD Mister Mim ! <john.r.sohn@gmail.com>
2020-09-15 09:14:28 -0600

    Reorganized directory

---
+++ ImportShapeFilesAndDBF/Fixfbfcrap/bin/Debug/MultiPointShape.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Fixfbfcrap
+{
+    public class MultiPointShape:BaseRecord
+    {
+        public MultiPointShape(BinaryReader br):base(br)
+        {
+
+        }
+    }
+}
