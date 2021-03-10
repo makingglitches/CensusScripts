4a45e5e62420c0effe9d1eb80c3d90c922f87a26
Magical Marvelous MADMADMAD Mister Mim ! <john.r.sohn@gmail.com>
2021-03-10 11:10:18 -0700

    And if they want to know who to blame for that repeat comment: their own kind of course. Snakes are often cannibalistic worms are too. and some forms of leeches.

---
+++ MinersAndPrograms/CensusFiles/Utilities/SpeciesRepackager.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+using System.IO;
+using CensusFiles.Loaders;
+using System.Data.SqlClient;
+
+namespace CensusFiles.Utilities
+{
+
+    // for some reason the gov packages species archives different than literally EVERY fucking thing they design.
+    public class SpeciesRepackager
+    {
+        public static void Repackage(string inputzipdir, string outputzipdir,SqlConnection scon)
+        {
+
+            inputzipdir = inputzipdir.Trim().EndsWith("\\") ? 
+                inputzipdir.Trim().Substring(0, inputzipdir.Trim().Length - 1):inputzipdir.Trim();
+
+            outputzipdir = outputzipdir.Trim().EndsWith("\\") ?
+               outputzipdir.Trim().Substring(0, outputzipdir.Trim().Length - 1) : outputzipdir.Trim();
+
+            string[] files = Directory.GetFiles(inputzipdir, "*.zip");
+
+
+            Console.WriteLine("Discovered " + files.Length.ToString() + " zips");
+
+            Dictionary<string, string> filekey = new Dictionary<string, string>();
+
+            foreach (string file in files)
+            {
+                Console.WriteLine("Processing " + Path.GetFileName(file));
+
+                ZipArchive za = ZipFile.OpenRead(file);
+
+                foreach (ZipArchiveEntry ze in za.Entries)
+                {
+                    
+                    if (ze.Name.ToLower().EndsWith(".zip"))
+                    {
+                        // earlier tests indicate all the species subcontents files have a prefix which is unqiue.
+                        // this could be used as a primary key.
+                        filekey.Add(Path.GetFileName(file), ze.Name.Split(new char[] { '_' })[0]);
+
+                        string tempdir = outputzipdir + "\\temp";
+                        string contentdir = tempdir + "\\contents";
+                        string repacked = outputzipdir + "\\" + Path.GetFileName(file);
+
+                        if (Directory.Exists(tempdir))
+                        {
+                            Directory.Delete(tempdir, true);
+                        }
+
+                        Directory.CreateDirectory(tempdir);
+                        Directory.CreateDirectory(contentdir);
+
+                        ze.ExtractToFile(tempdir+"\\temp.zip");
+
+                        ZipFile.ExtractToDirectory(tempdir+"\\temp.zip", contentdir);
+
+                        if (File.Exists(repacked))
+                        {
+                            File.Delete(repacked);
+                        }
+
+                        // so you know what my magnum opus was ?
+                        // I got a fuck ton of pedophiles stuck who were trying to heartlessly steal
+                        // my creations and work and pass it off as their own 
+                        // as these completely immoral garbage tend to with everything because they're souless
+                        // meanwhile the garbage that keeps trying to entrap me and delete decades of life is old
+                        // fat and in their own kind of prison, and they'll die there.
+                        // and was this at an acceptable cost ? no not really. 
+                        // these weird little dont give a damn about anyone or anything garbage fucks 
+                        // are a disease and that disease was/is spreading because their parents are the same.
+                        // in a way discovering my own parents were garbage that were pretending otherwise
+                        // was a tad better for me morally. but still not ideal because the world doesnt need
+                        // to be filled with thrillkilling thrillraping garbage that wants everyone to suffer
+                        // or just doesnt have ANY standards or values whatsoever.
+                        // we've had quite enough of those little whores.
+
+                        ZipFile.CreateFromDirectory(contentdir, 
+                                                   repacked);
+                        
+                    }
+                }
+
+
+
+            }
+
+            var s = File.Create(outputzipdir + "\\keys.txt");
+
+            StreamWriter sw = new StreamWriter(s);
+
+            foreach (KeyValuePair<string, string> kvp in filekey)
+            {
+                sw.WriteLine(kvp.Key + "\t" + kvp.Value);
+            }
+
+            sw.Flush();
+            s.Flush();
+            s.Close();
+            // how many times this long time period has passed, no idea, but everyone is freaked out and scared
+            // when i end up back somewhere else, who knows if its an act or theyre being awoken from 
+            // a very long slumber if that is even the case but they certainly all seem MORE miserable
+            // and less alive everytime i see any of them before i even comment on these things.
+            // shame i have had to continually reproduce this/
+            // noone really explained any of the fucked up rules that allow me to avoid them playing
+            // child molester drinking games with one another to do literally anything so i'm kind of stuck it would seem
+            Console.WriteLine("wrote keys file.");
+
+        }
+    }
+}
--- MinersAndPrograms/ImportShapeFilesAndDBF/Fixfbfcrap/Program.cs
+++ MinersAndPrograms/ImportShapeFilesAndDBF/Fixfbfcrap/Program.cs
@@ -12,302 +12,63 @@ using System.Data.SqlTypes;
 using System.Data.Common;
 using System.Data;
 using CensusFiles.Loaders;
-
+using CensusFiles.Utilities;
+using System.IO.Compression;
 
 namespace Fixfbfcrap
 {
     class Program
     {
-        //  i need more fucking ram !
-        // course this worked fine yesterday with no resources semaphore wait at approx 80% memory usage
-        // at 1000 records per.. so fuck off assholes.
-        //... still. ass fucking holes stop dragging me back trying to make me act like the rest of these labotomized assholes are correct !
-        // instead of manufacturing crimes to entrap me that make no sense just to delay me, why not do what you people should have done
-        // if you were really security minded and just send me some chick to screw and make it so i work in a cabin somewhere writing code in th evenings
-        // etc.. jesus, i'd never have complained or noticed anything you people were doing if you werent fucking advertising so hard
-        // and additionally no i wont sit here and say extra years of my life werent dragged out of me
-        // while the rest of these idiots slowly became basketweaving madmen 
-        // seriously though goddamn if you people were actually hiding something you wouldnt just wave it around where anyone can see and say
-        // 'whoa... thats really odd and disconcerting and they are acting like TOTAL FUCKING FREAKS'
-        // and invent the lie that this is the way it always was and spout that shit to someone else mother fuckers.
-        // go hang with my chomo fucking dad if you people want to feel 'normal', he was one of the 1970s loser club.
-  
+
+        // god i love source control
+        
         static void Main(string[] args)
         {
-            //string dir = @"C:\Users\John\Documents\CensusProject\CensusShapeFileData\RiversAndStreamsData";
-
-            //RiversLoader rl = new RiversLoader(true, false, dir);
-
-            //rl.Options.RecordLimit = 100;
-
-            //rl.LoadZips();
-
-
-            // apparently tori wanted to save time for whatever piece of crap keep stealing my work to 
-            // get some rape in. poor poor lad.
-            // motherfuckers = them
-            // seriously all this waste and time that could have been better spent working on something constructive and they say
-            // 'oh we know his daddy, and he wasnt raised to know shit about our true awful fucked up self destructive and purely destructive natures
-            // 'i know we can take advantage of him and try to set him up to get stuck so he will be susceptible to our "evil empire" bullshit
-            // 'and thereby we can scare or traumatize him into making stuff we steal constantly and then turn shit around so he wont want anything
-            // 'later on while we're in the process of fucking ourselves over'
-            // which apparnetly they are doing.
-            // and the world too.
-
-            //string roaddir = @"C:\Users\John\Documents\CensusProject\CensusShapeFileData\RoadsZips";
-
-            //RoadsLoader roadl = new RoadsLoader(true, false, roaddir);
-
-            //roadl.Options.RecordLimit = 1000;
-
-            //roadl.LoadZips();
-
-
-            //string countydir = @"C:\Users\John\Documents\CensusProject\CensusShapeFileData\CountyZips";
-            //CountyLoader col = new CountyLoader(false, true, countydir);
-            //col.Options.RecordLimit = 1000;
-            //col.LoadZips();
-
-            //string aqudir = @"C:\Users\John\Documents\CensusProject\CensusShapeFileData\AqiferData";
-            //AquiferLoader aql = new AquiferLoader(false, true, aqudir);
-            //aql.LoadZips();
-
-            //string placedir = @"C:\Users\John\Documents\CensusProject\CensusShapeFileData\PlacesZips";
-            //PlaceLoader pl = new PlaceLoader(false, true, placedir);
-            //pl.Options.RecordLimit = 1000;
-            //pl.LoadZips();
-
-
-            string statedir = @"C:\Users\John\Documents\CensusProject\CensusShapeFileData\StateZips";
-            StateLoader sl = new StateLoader(false, true, statedir);
-            sl.LoadZips();
-
-            // nope.. instead i'm sitting in poverty doing my best not to go nutty.
-            // which is hard when everyone in a 1 state radius is fucking insane.
 
 
 
-            #region OldCommentedCode
+            string locspecdir = @"C:\Users\John\Documents\CensusProject\CensusShapeFileData\SpeciesData";
 
-            //SqlConnectionStringBuilder scb = new SqlConnectionStringBuilder();
-            //scb.InitialCatalog = "Geography";
-            //scb.IntegratedSecurity = true;
+            SpeciesRepackager.Repackage(locspecdir, locspecdir + "\\repack",null);
 
-            //SqlConnection scon = new SqlConnection(scb.ConnectionString);
-            //scon.Open();
+            //string[] files = Directory.GetFiles(locspecdir, "*.zip");
 
-            //DataTable dt = RiversRecord.GetTable(scon);
+            //List<string> containedzip = new List<string>();
 
-            //SqlBulkCopy sb = new SqlBulkCopy(scon);
+            //bool end = false;
 
-            //sb.DestinationTableName = "Rivers";
-            //sb.WriteToServer(dt);
-
-            //scon.Close();
-
-            //// ok lets look into record 272
-            //var shp = new ShapeFile(@"C:\Users\John\Documents\CensusProject\Issues\tl_2019_01_place\tl_2019_01_place.shp");
-            //shp.Load();
-
-            //var r = shp.Records[271];
-
-            //string wkt = r.Record.GetWKT();
-
-            //PolygonShape p = (PolygonShape)r.Record;
-
-            //StreamWriter sw = new StreamWriter("Citronell Point Data.txt");
-
-            //if (p.NumParts==1)
+            //foreach (string f in files)
             //{
-            //    sw.WriteLine("File only has one part.");
-            //}
-
-            //sw.WriteLine("Shape contains " + p.NumPoints.ToString() + " points.");
-            //sw.WriteLine("with " + p.NumParts.ToString() + " parts.");
-
-            //int partindex = 0;
-
-            //for (int x=0; x < p.NumPoints; x++)
-            //{
-            //    if (partindex < p.NumParts &&  x==p.Parts[partindex])
-            //   {
-            //        sw.WriteLine();
-            //        sw.WriteLine("Part starting at point #" + x);
-            //        partindex++;
-            //   }
-            //    sw.WriteLine(p.Points[x].X.ToString() + ", " + p.Points[x].Y.ToString());
-            //}
-
-            //sw.WriteLine();
-            //sw.WriteLine(wkt);
-            //sw.Flush();
-            //sw.Close();
-
-
-
-            //var dread = new DbfDataReader.DbfDataReader(@"C:\Users\John\Documents\CensusProject\AqiferData\aquifrp025.dbf");
-
-
-
-            //ClassGenerator.WriteClassBase("AquiferBase", "AquiferBase.cs", dread);
-
-            //var shp = new ShapeFile(@"C:\Users\John\Documents\CensusProject\AqiferData\aquifrp025.shp");
-            //shp.Load();
-
-            //var dread = new DbfDataReader.DbfDataReader(@"C:\Users\John\Documents\CensusProject\RiversAndStreamsData\USA_Rivers_and_Streams-shp\9ae73184-d43c-4ab8-940a-c8687f61952f2020328-1-r9gw71.0odx9.dbf");
-
-
-
-            //ClassGenerator.WriteClassBase("RiversBase", "RiversBase.cs", dread);
-
-            //var shp = new ShapeFile(@"C:\Users\John\Documents\CensusProject\RiversAndStreamsData\USA_Rivers_and_Streams-shp\9ae73184-d43c-4ab8-940a-c8687f61952f2020328-1-r9gw71.0odx9.shp");
-            //shp.Load();
-
-            //var dread = new DbfDataReader.DbfDataReader(@"C:\Users\John\Documents\CensusProject\CensusShapeFileData\StateZips\tl_2019_us_state\tl_2019_us_state.dbf");
-
-
-            //ClassGenerator.WriteClassBase("StateBase", "StateBase.cs", dread);
-
-            //var dread = new DbfDataReader.DbfDataReader(@"C:\Users\John\Documents\CensusProject\CensusShapeFileData\CountyZips\tl_2019_us_county\tl_2019_us_county.dbf");
-
-
-            //ClassGenerator.WriteClassBase("CountyBase", "CountyBase.cs", dread);
-
-            // string shpfiledir = @"C:\Users\John\Documents\CensusProject\QrCode\Input\Places2019\Places";
-            // string sampleshpfile = @"C:\Users\John\Documents\CensusProject\QrCode\Input\Places2019\Places\tl_2019_01_place.shp";
-
-            //var shpfiles =  Directory.GetFiles(shpfiledir, "*.shp");
-
-            // foreach (string filename in shpfiles)
-            // {
-            //     ShapeFile s = new ShapeFile(filename);
-            //     s.Load();
-
-            //     int nonpoly = 0;
-
-            //     foreach (var r in s.Records)
-            //     {
-            //         if (!(r.Record is PolygonShape))
-            //         {
-            //             nonpoly++;
-            //         }
-            //     }
-
-            //     if(nonpoly >0)
-            //     {
-            //         Console.WriteLine(filename + "  contains " + nonpoly.ToString() + " records.");
-            //     }
-            // }
-
-
-            // string roaddir = @"C:\Users\John\Documents\CensusProject\QrCode\Input\Places2019\Roads";
-            // string roaddbf = @"C:\Users\John\Documents\CensusProject\QrCode\Input\Places2019\Roads\tl_2019_01001_roads.dbf";
-            // string roadshp = @"C:\Users\John\Documents\CensusProject\QrCode\Input\Places2019\Roads\tl_2019_01001_roads.shp";
-
-            // SqlConnectionStringBuilder scb = new SqlConnectionStringBuilder();
-            // scb.InitialCatalog = "Geography";
-            // scb.IntegratedSecurity = true;
-
-            // SqlConnection scon = new SqlConnection(scb.ConnectionString);
-            // scon.Open();
-
-            // var roaddbfrecords = RoadRecord.ParseDBFFile(roaddbf, scon,true);
-
-            // ShapeFile road = new ShapeFile(roadshp);
-            // road.Load();
-
-            //Console.WriteLine( road.Records[0].Record.GetWKT());
-
-
-            // DbfDataReader.DbfDataReader dread = new DbfDataReader.DbfDataReader(roaddbf);
-
-            //ClassGenerator.WriteClassBase("RoadBase", "RoadBase.cs", dread);
-
-            //  Console.WriteLine(s.Records[0].Record.GetWKT());
-
-            //SqlConnectionStringBuilder scb = new SqlConnectionStringBuilder();
-            //scb.InitialCatalog = "Geography";
-            //scb.IntegratedSecurity = true;
-
-            //SqlConnection scon = new SqlConnection(scb.ConnectionString);
-            //scon.Open();
-
-            //string sampledbffile = @"C:\Users\John\Documents\CensusProject\QrCode\Input\Places2019\Places\tl_2019_01_place.dbf";
-
-            //var places = PlaceRecord.ParseDBFFile(sampledbffile,scon);
-
-
-
-            //SqlConnectionStringBuilder scb = new SqlConnectionStringBuilder();
-            //scb.InitialCatalog = "Geography";
-            //scb.IntegratedSecurity = true;
-
-            //SqlConnection scon = new SqlConnection(scb.ConnectionString);
-            //scon.Open();
-
-            //SqlCommand sq = new SqlCommand("select top 100 * from dbo.FipsKeys", scon);
-
-            //SqlDataReader sr = sq.ExecuteReader();
-
-            //ClassGenerator.WriteClassBase("FipsKeyBase", "FipsKeyBase.cs", sr);
-
-            //sr.Close();
-
-            //scon.Close();
-
-
-
-            //string sampledbffile = @"C:\Users\John\Documents\QrCode\Input\Places2019\Places\tl_2019_01_place.dbf";
-
-            //DbfDataReader.DbfDataReader dr = new DbfDataReader.DbfDataReader(sampledbffile);
-
-            //PlaceRecord pr = new PlaceRecord();
-            //dr.Read();
-            //pr.Read(dr);
-
-
-
-            //CensusFiles.ClassGenerator.WriteClassBase("PlaceBase","PlaceBase.cs", dr);
-
-
-            //string sampleshpfile = @"C:\Users\John\Documents\QrCode\Input\Places2019\Places\tl_2019_01_place.shp";
-
-            //ShapeFile s = new ShapeFile(sampleshpfile);
-
-            //s.Load();
-
-            //string[] s = Directory.GetFiles(@"C:\Users\John\Documents\QrCode\ImportShapeFilesAndDBF\ShapeUtilities", "*.cs");
-
-            //var fs = File.Create("ifstatements.cs");
-            //var ts = new StreamWriter(fs);
-
-            //foreach (string s1 in s)
-            //{
-            //    var s2 = Path.GetFileNameWithoutExtension(s1);
-
-            //    ts.WriteLine("if (this is " + s2 + ")");
-            //    ts.WriteLine("{");
-            //    ts.WriteLine("  var obj=(" + s2 + ")this;");
-            //    ts.WriteLine("  sb.Append(\"" + s2.ToUpper().Replace("SHAPE", "") + "(\");");
-            //    ts.WriteLine("}");
-            //    ts.WriteLine();
-
-
+            //    ZipArchive za = ZipFile.OpenRead(f);
+
+            //    foreach (ZipArchiveEntry s in za.Entries)
+            //    {
+            //        if (s.Name.ToLower().EndsWith(".zip"))
+            //        {
+            //            if (containedzip.Contains(s.Name.ToLower()))
+            //            {
+            //                end = true;
+            //                Console.WriteLine("Found duplicate subarchive name");
+            //                break ;
+            //            }
+            //            else
+            //            {
+            //                containedzip.Add(s.Name.ToLower());
+            //            }
+            //        }
+            //    }
+
+            //    if (end)
+            //    {
+            //        break;
+            //    }
 
             //}
 
-            //ts.Flush();
-            //ts.Close();
-            #endregion OldCommentedCode
+            //if (!end) Console.WriteLine("No Duplicates found.");
+
+            //Console.ReadLine();
         }
-
-       
-      
-        // as much as i get sick of rewriting the same fucking code I get sick of having to constantly attempt to pretend
-        // that there is some rhyme or reason to this
-        // these people tell half truths mostly but a few whole ones
-        // and they live their lives in an artificial circleS
-      
        
     }
 }
