18344e95c228f8b14b802729e2eb0905933fd099
Magical Marvelous MADMADMAD Mister Mim ! <john.r.sohn@gmail.com>
2021-03-22 14:29:04 -0600

    memories of zimmerman telling me these smug satisifed people are only as they are because they vent their most disgusting desires. and here i just thought they were ONLY superficial arrogant egotistical hypocritical garbage lol

--- MinersAndPrograms/ImportShapeFilesAndDBF/Fixfbfcrap/Program.cs
+++ MinersAndPrograms/ImportShapeFilesAndDBF/Fixfbfcrap/Program.cs
@@ -26,45 +26,50 @@ namespace Fixfbfcrap
         static void Main(string[] args)
         {
 
-            string zipdir = @"C:\Users\John\Documents\CensusProject\CensusShapeFileData\SpeciesData\repack";
-            string sfs = @"c:\testdata";
 
-            string[] shpfiles = Directory.GetFiles(sfs, "*.shp");
+            // this is due to conus albers projection, not data error, working on getting gdal built because of config mismatches
+            // with swig libs packaged, or fixing that issue, so hopefully conversions can occur more easily
+            // also to get the tree canopy and other elevation data file readers set up.
+            // the img format with ige spillfill is not compressed AT ALL. and it very likely could be losslessly compressed !
+            //string zipdir = @"C:\Users\John\Documents\CensusProject\CensusShapeFileData\SpeciesData\repack";
+            //string sfs = @"c:\testdata";
 
-            foreach (string f in shpfiles)
-            {
-                ShapeFile s = new ShapeFile(f);
-                s.Load();
+            //string[] shpfiles = Directory.GetFiles(sfs, "*.shp");
 
-                Console.Write(f);
+            //foreach (string f in shpfiles)
+            //{
+            //    ShapeFile s = new ShapeFile(f);
+            //    s.Load();
 
-                int countlon = 0;
-                int countlat = 0;
+            //    Console.Write(f);
 
-                for (int x = 0; x < s.Records.Count; x++)
-                {
-                    PolygonShape ps = (PolygonShape)s.Records[x].Record;
+            //    int countlon = 0;
+            //    int countlat = 0;
 
-                    for (int y = 0; y < ps.NumPoints; y++)
-                    {
+            //    for (int x = 0; x < s.Records.Count; x++)
+            //    {
+            //        PolygonShape ps = (PolygonShape)s.Records[x].Record;
 
-                        countlon += (ps.Points[y].X > 180 || ps.Points[y].X < -180) ? 1 : 0;
-                        countlat += (ps.Points[y].Y > 90 || ps.Points[y].Y < -90) ? 1 : 0;
+            //        for (int y = 0; y < ps.NumPoints; y++)
+            //        {
+
+            //            countlon += (ps.Points[y].X > 180 || ps.Points[y].X < -180) ? 1 : 0;
+            //            countlat += (ps.Points[y].Y > 90 || ps.Points[y].Y < -90) ? 1 : 0;
                      
-                    }
+            //        }
 
-                }
+            //    }
 
-                if (countlat > 0 || countlon > 0)
-                {
-                    Console.WriteLine("File Contained " + countlat.ToString() + " bad latitude points and " + countlon.ToString() + " bad longitude points.");
-                }
-                else
-                {
-                    Console.WriteLine("File within bounds");
-                }
+            //    if (countlat > 0 || countlon > 0)
+            //    {
+            //        Console.WriteLine("File Contained " + countlat.ToString() + " bad latitude points and " + countlon.ToString() + " bad longitude points.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("File within bounds");
+            //    }
 
-            }
+            //}
 
             //string f = @"C:\Users\John\Documents\CensusProject\CensusShapeFileData\SpeciesData\repack\temp\contents\mBCPMx_CONUS_Range_2001v1.dbf";
 
