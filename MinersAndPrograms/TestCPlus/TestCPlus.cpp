7e35a1327854e59ab86375e92e2de3e21a2254cb
Magical Marvelous MADMADMAD Mister Mim ! <john.r.sohn@gmail.com>
2021-03-23 21:37:11 -0600

    with a little thought i seem to remember that jupyter notebooks were not as useful as i would have hoped for.
    at least i got the code sort of working.
    need to see about sectioning off the raster data which appears to be entirely color based indicative of more or less canopy data in the case of the treecanopy source, which on being visualized in qgis appears pretty goddamn accurate !
    
    however its uncompressed and it could probably be reduced down considerably !

--- MinersAndPrograms/TestCPlus/TestCPlus.cpp
+++ MinersAndPrograms/TestCPlus/TestCPlus.cpp
@@ -2,30 +2,59 @@
 //
 
 #include <iostream>
-#include "gdal.h"
+//#include "gdal.h"
+#include "gdal_priv.h"
+#include "cpl_conv.h"
 
 int main()
 {
-    // so maybe we do somenthing different now :P
-    // since nothing changes stupid lol
-    std::cout << "Hello World!\n";
 
+    // so seriously all they need to do is make sure i leave before i get trapped. so thats before 2017, move on or move on now really
+    // they're just pretending to be threatened because they spent x number of years of my life rubbing this shit in my face 
+    // which led to alot of very bad things for them and for me.
+    // circular living. what a fucking great idea !
+    // i get stuck because they engineered it so.
+    // they want me to stay. dont know why because they wont be getting shit.
+    // and btw, i've seen plenty of people who do not swear or cuss or use profanity who are complete dullards
+    // i'm pissed off and tired and i hate everyone here and dont feel like working on code i already figured out before
+    // and btw the c# version of this worked fucking fine !
+   
+    std::cout << "Hello World!\n";
     
     const char* filename = R"(C:\Users\John\Documents\CensusProject\CensusShapeFileData\TreeCanopy\nlcd_2016_treecanopy_2019_08_31\nlcd_2016_treecanopy_2019_08_31.img)";
     
+   
+    GDALDataset* poDataset;
     GDALAllRegister();
+    
+    poDataset = (GDALDataset*)GDALOpen(filename, GA_ReadOnly);
+    
+    GDALRasterBand* band = poDataset->GetRasterBand(1);
 
-    GDALDatasetH  g = GDALOpen(filename, GA_ReadOnly);
+   
+
+    std::cout << poDataset->GetRasterCount() << "\n";
+
+    std::cout << poDataset->GetProjectionRef() << "\n";
+    std::cout << poDataset->GetDriver()->GetDescription() << "\n";
+
+    GDALDataset::Bands b =   poDataset->GetBands();
+    std::cout << b.size() << "\n";
+    std::cout << poDataset->GetFileList() << "\n";
+
+    std::cout << poDataset->GetGCPProjection() << "\n";
+
+    // so once again jupyter for learning this seems smart.
+    // i always have so many scattered thoughts and drawing them together with gradual meat 
+    // and bone example seems useful still....
+    // course the tool may not have quite the functionality i'm looking for though...
+    // to be useful to me it would seem to need to be able to represent a whole range of things
+    // and pull together at the very least c++ and mssql connections+queries so that
+    // and allow me to add links to useful informations surrounding the concept
+    // as i am remembering looking at this its unclear if it was useless or if i just didnt 
+    // push enough in between them wasting my time and bitching at each other.
+
+
 
 }
 
-// ryan he says. awful jackass still.
-// they just gotta keep on trying to make time stand still
-// why ?
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
