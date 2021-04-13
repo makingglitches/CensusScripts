798fdad15057d882f92f5318d80841b11a349218
Magical Marvelous MADMADMAD Mister Mim ! <john.r.sohn@gmail.com>
2021-04-12 20:51:57 -0600

    god i hate c++. especially on windows. take a wild guess once the wrapper worked what i got to work right the fing way !

--- MinersAndPrograms/ImportShapeFilesAndDBF/Fixfbfcrap/Program.cs
+++ MinersAndPrograms/ImportShapeFilesAndDBF/Fixfbfcrap/Program.cs
@@ -17,6 +17,8 @@ using System.IO.Compression;
 using System.Xml;
 using OSGeo.GDAL;
 using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+using System.Drawing;
 
 namespace Fixfbfcrap
 {
@@ -38,7 +40,7 @@ namespace Fixfbfcrap
             // and yeah now remember the end of the season because zimmerman commented on that one.
             // a very long time ago it seems.
 
-            string file = @"C:\Users\John\Documents\CensusProject\CensusShapeFileData\TreeCanopy\nlcd_2016_treecanopy_2019_08_31";
+            string file = @"C:\Users\John\Documents\CensusProject\CensusShapeFileData\TreeCanopy\nlcd_2016_treecanopy_2019_08_31\nlcd_2016_treecanopy_2019_08_31.img";
 
             //  SetDllDirectory(@"C:\OSGeo4W64\apps\gdal\csharp\csharp");
 
@@ -47,12 +49,66 @@ namespace Fixfbfcrap
             // i've set the path here to search on using the kernel32 wrapper above
 
             // i changed the directory to location of the dll.
-            Environment.CurrentDirectory = @"C:\OSGeo4W64\apps\gdal\csharp\csharp";
-            
+            //  Environment.CurrentDirectory = @"C:\OSGeo4W64\apps\gdal\csharp\csharp";
+            Gdal.AllRegister();
+
             //and isnt it interesting it can't find a dll in teh same goddamn directory
-            Gdal.Open(file, Access.GA_ReadOnly);
-      
+            Dataset d = Gdal.Open(file, Access.GA_ReadOnly);
+
+
+            var b = d.GetRasterBand(1);
+            var ct = b.GetColorTable();
+            double[] geo = new double[6];
+
+            d.GetGeoTransform(geo);
+
+
+            var bounds = new { Left = geo[0], Top = geo[3], xsize = geo[1], ysize = geo[5] };
+
+
+            byte[] bytes = new byte[512 * 512];
+
+
+            b.ReadRaster(d.RasterXSize/2, d.RasterYSize/2, 512, 512, bytes, 512, 512, 0, 0);
+
+            System.Drawing.Bitmap dest = new System.Drawing.Bitmap(512, 512);
+
+            Console.WriteLine(ct.GetCount());
+
+            for (int x = 0; x < 512; x++)
+            {
+                for (int y=0; y < 512; y++)
+                {
+                    var index = bytes[y * 512 + x];
+                    var col = ct.GetColorEntry(index);
+                    var c = Color.FromArgb(col.c4, col.c1, col.c2, col.c3);
+                    dest.SetPixel(x, y, c);
+                }
+            }
+
+            
+            ImageCodecInfo pnginfo = ImageCodecInfo.GetImageEncoders().Where(o => o.FormatID.Equals(ImageFormat.Png.Guid)).First();
+
+            var compop = System.Drawing.Imaging.Encoder.Compression;
+            var qualop = System.Drawing.Imaging.Encoder.Quality;
+
+            
+            FileStream fs = new FileStream("test.png", FileMode.Create, FileAccess.Write, FileShare.None);
+
+            dest.Save(fs, ImageFormat.Png);
+
+            fs.Flush();
+            fs.Close();
+
+            // its not just that csharp is better
+            // it looks nicer
+            // the code is cleaner
+            // and tracking down what is wrong is much easier !!!
+            // I mean this is still using the same lowlevel functions but its NICE! 
+            // still !
+
+
         }
-       
+
     }
 }
